b31b5273fb85f056fdb639e5c37cd61cb1909dfa
John <john@onejohn.org>
2017-04-22 21:14:52 -0400

    Task 2 complete

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,16 +1,18 @@
 class QuestionsController < ApplicationController
   def show
-    question = Question.includes(:answers, :user).find(params[:id])
+    question = Question.includes(:answers, :user).where("id = ? and private = ?", params[:id], false).take
     
     output = question.as_json
-    output["user_name"] = question.user.name
+    if question
+      output["user_name"] = question.user.name
 
-    output["answers"] = []
+      output["answers"] = []
 
-    question.answers.each do |answer|
-      answer_hash = answer.as_json
-      answer_hash["user_name"] = answer.user.name
-      output["answers"].push(answer_hash)
+      question.answers.each do |answer|
+        answer_hash = answer.as_json
+        answer_hash["user_name"] = answer.user.name
+        output["answers"].push(answer_hash)
+      end
     end
     render :json => output 
 
