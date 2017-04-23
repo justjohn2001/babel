ba9e01c910bcc83a0d71c422cc31732458d03267
John <john@onejohn.org>
2017-04-22 20:35:10 -0400

    playing with records

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,13 @@
 class QuestionsController < ApplicationController
   def show
-    @question = Question.find(params[:id])
-    render :json => @question
+    question = Question.includes(:answers, :user).find(params[:id])
+    
+    output = question.to_json
+
+    question.answers.each do |answer|
+      output = output + "<br>" + answer.to_json
+    end
+    render :html => output 
+
   end
 end
